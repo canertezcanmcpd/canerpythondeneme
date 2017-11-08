5008ecc64422bbd2ac911b816c14ffd7f69500d6
canertezcanmcpd <canertezcan@gmail.com>
2017-11-08 10:20:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-
+# -*- coding:utf8 -*-
+# !/usr/bin/env python
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from __future__ import print_function
 from future.standard_library import install_aliases
@@ -37,28 +51,8 @@ def webhook():
 
 def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
-        return {
-        "speech": "Zorsa yaparız imkansızsa zaman alır.",
-        "displayText": "Zorsa yaparız imkansızsa zaman alır.",
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
-   else	
-		return caner()
-
-
-def caner ():
-	 return {
-        "speech": "Zorsa yaparız imkansızsa zaman alır.",
-        "displayText": "Zorsa yaparız imkansızsa zaman alır.",
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
-		
-def yahooWeatherForecast ():
-	baseurl = "https://query.yahooapis.com/v1/public/yql?"
+        return {}
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
@@ -67,7 +61,8 @@ def yahooWeatherForecast ():
     data = json.loads(result)
     res = makeWebhookResult(data)
     return res
-	
+
+
 def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
