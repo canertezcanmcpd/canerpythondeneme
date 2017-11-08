1def9fb008bb66b3c227698c19a8b0c6813b7776
canertezcanmcpd <canertezcan@gmail.com>
2017-11-08 14:17:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -52,8 +52,7 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {"speech": "zorsa yaparız imkansızsa zaman alır","displayText": "zorsa yaparız imkansızsa zaman alır deneme","source": "apiai-weather-webhook-sample"}
-	elif req.get("result").get("action") != "yahooWeatherForecast":
-	{
+	elif req.get("result").get("action") == "yahooWeatherForecast":	
 		baseurl = "https://query.yahooapis.com/v1/public/yql?"
 		result = req.get("result")
 		parameters = result.get("parameters")
@@ -65,7 +64,7 @@ def processRequest(req):
 		data = json.loads(result)
 		res = makeWebhookResult(data)
 		return res
-	}
+	
 		
 		
 
