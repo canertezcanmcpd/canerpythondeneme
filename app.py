1f6f821122cffb85948b529521501c4b4b259611
canertezcanmcpd <canertezcan@gmail.com>
2017-11-08 11:48:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -61,19 +61,7 @@ def processRequest(req):
     data = json.loads(result)
     res = makeWebhookResult(data)
     return res
-		
 
-
-def asd():
-	 baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
 		
 def makeYqlQuery(req):
     result = req.get("result")
