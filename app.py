3388bd11af45785ec350ebd87b9907a4577dfcdf
canertezcanmcpd <canertezcan@gmail.com>
2017-11-09 14:01:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -68,8 +68,11 @@ def processRequest(req):
 
 
 def claims():	
-    baseurl = 'http://asknnapi.azurewebsites.net/api/contact/ClaimsStatus?'	    
-    yql_url = baseurl + urlencode({'identityNumber': '3454'}) + "&format=json"
+    baseurl = 'http://asknnapi.azurewebsites.net/api/contact/ClaimsStatus?'	 
+    result = req.get("result")
+    parameters = result.get("parameters")
+    identityNumber = parameters.get("identityNumber")
+    yql_url = baseurl + urlencode({'identityNumber': identityNumber}) + "&format=json"
     result = urlopen(yql_url).read()
     data = json.loads(result)
     return {"speech": data,"displayText": data,"source": "apiai-weather-webhook-sample"}
