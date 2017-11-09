a808fe8cc72b5ce64e92ba6c37f272f1711cd9cf
canertezcanmcpd <canertezcan@gmail.com>
2017-11-09 15:17:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def claims():
     yql_url = baseurl + urlencode({'identityNumber': "23"}) + "&format=json"
     result = urlopen(yql_url).read()
     data = json.loads(result)
-    return {"speech": data,"displayText": data,"source": "apiai-weather-webhook-sample"}
+    return {"speech": urlencode(data),"displayText": data,"source": "apiai-weather-webhook-sample"}
 			
 
 def makeYqlQuery(req):
